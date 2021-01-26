b20ad6e0829fecc000a95eacd77b493f0018eaf9
Ben Bolthouse <benbolthouse@gmail.com>
2021-01-26 10:51:29 -0700

    Add environment specific routing in backend to serve react index

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,37 @@
+const express = require('express');
+let { environment: env } = require('../config');
+
 const router = require('express').Router();
 
+
+// automatically send xsrf cookie token in development environment for react debugging
+if (env !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('xsrfToken', req.csrfToken());
+    return res.json({message: 'CSRF cookie sent with response'});
+  });
+}
+
+// mount api routes after csrf restore route
 router.use('/api', require('./api'));
 
-// React routes
-// router.get('/token-me', (req, res, next) => {
-//   res.cookie('xsrfToken', req.csrfToken());
-//   res.send("You've been tokened!!!");
-// });
+// serves the react index in production along with xsrf cookie token
+if (env === 'production') {
+  const path = require('path');
+
+  // serve react as default / route
+  router.get('/', (req, res) => {
+    res.cookie('xsrfToken', req.csrfToken());
+    return res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'));
+  });
+
+  router.use(express.static(path.resolve('../frontend/build')));
+
+  // serve react index as default /api route
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('xsrfToken', req.csrfToken());
+    return res.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'));
+  });
+}
 
 module.exports = router;
