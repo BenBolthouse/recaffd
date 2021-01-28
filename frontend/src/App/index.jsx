71e0847d4b89e8ce5e24332888a42a4ed1271da1
Ben Bolthouse <benbolthouse@gmail.com>
2021-01-27 23:03:30 -0700

    Add dispatch dependency to useEffect in App component

--- frontend/src/App/index.jsx
+++ frontend/src/App/index.jsx
@@ -24,7 +24,7 @@ const App = () => {
     dispatch(sessionActions.restoreUser())
       .then(() => setIsLoaded(true))
       .catch(() => setIsLoaded(true))
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
     <>
