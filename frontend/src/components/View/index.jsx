09b7fe9bba45081913fa08b12a7d331b4942f885
Ben Bolthouse <benbolthouse@gmail.com>
2021-01-28 00:03:56 -0700

    Add comments and integrate AppMessages components

--- frontend/src/components/View/index.jsx
+++ frontend/src/components/View/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+
+// Component includes
 import Navbar from '../Navbar';
 import Footer from '../Footer';
+import AppMessages from '../AppMessages'
 
+// Scoped styles
 import './styles.css';
 import './pattern.css';
 
+/**
+ * Render a default view.
+ */
 export const View = ({ children, isLoaded }) => {
   return (
     <div className="page-wrapper">
+      <AppMessages />
       <div className="page">
+
         {children}
         <Footer isLoaded={isLoaded} />
       </div>
@@ -16,9 +25,13 @@ export const View = ({ children, isLoaded }) => {
   )
 }
 
+/**
+ * Render a view with a form container placed dead center on the screen.
+ */
 export const FormView = ({ children, isLoaded }) => {
   return (
     <div className="page-wrapper">
+      <AppMessages />
       <div className="page">
         <div className="view form-view">
           {children}
@@ -29,11 +42,15 @@ export const FormView = ({ children, isLoaded }) => {
   )
 }
 
+/**
+ * Render a default view with padding for the navbar.
+ */
 export const ViewWithNavigation = ({ children, isLoaded }) => {
   return (
     <>
       <Navbar isLoaded={isLoaded} />
       <div className="page-wrapper with-navbar">
+        <AppMessages />
         <div className="page">
           {children}
           <Footer isLoaded={isLoaded} />
