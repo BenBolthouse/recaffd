5aa1f3c15cd3a971cf811e107226f634713a7679
Ben Bolthouse <benbolthouse@gmail.com>
2021-01-29 13:31:42 -0700

    Add feeds router to api router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const usersRouter = require('./users')
 const sessionsRouter = require('./sessions')
+const feedsRouter = require('./feeds')
 
 router.use('/users', usersRouter);
 router.use('/sessions', sessionsRouter);
+router.use('/feeds', feedsRouter);
 
 // 404 handler for API requests
 router.all('/*', (_req, res) => {
