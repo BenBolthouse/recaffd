cb8325b3596596b25bffa01956fb98600b65457c
Ben Bolthouse <benbolthouse@gmail.com>
2021-01-29 13:46:16 -0700

    Create HomePage component and link up to Redux state

--- frontend/src/components/HomePage/index.jsx
+++ frontend/src/components/HomePage/index.jsx
@@ -1,33 +1,74 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+
+// Components
+import VisibilitySensor from 'react-visibility-sensor';
+
+// Custom hooks
 import { useDidMountEffect } from '../../hooks';
+
+// TODO: delete next line
 import mockProducts from '../../.local_mock/products';
+
+// State actions
+import * as feedsActions from '../../store/feeds';
 
 // Scoped styles
 import './styles.css';
 
 
 const HomePage = () => {
-  const [feedItems, setFeedItems] = useState([]);
-  const quantityPerFetch = 15;
+  // Hooks
+  const dispatch = useDispatch();
+  const feed = useSelector(s => s.feeds.view);
+  
+  // Component state
+  const isMounted = useRef(false);
+  const [location, setLocation] = useState('view');
+  const [includeProducts, setIncludeProducts] = useState(false);
+  const [includeBusinesses, setIncludeBusinesses] = useState(true);
+  const [offset, setOffset] = useState(0);
+  const [sortedBy, setSortedBy] = useState('nameAlphabeticalDesc');
+  const [limit, setLimit] = useState(20);
+  const [appendCount, setAppendCount] = useState(0);
 
   useEffect(() => {
-    const fetchedItems = mockProducts.slice(0, quantityPerFetch);
-    setFeedItems(fetchedItems);
-    console.log(mockProducts);
-  }, [mockProducts]);
+    console.log('useEffect...');
+    console.log('location', location);
+    dispatch(feedsActions.fetchCustomFeed({
+      location,
+      sortedBy,
+      offset,
+      limit,
+      includeProducts,
+      includeBusinesses,
+    }))
+    setOffset(o => offset + limit);
+  }, [appendCount]);
 
+  const scrolledToBottom = e => {
+    console.log('Bottom of list hath been sroll\'d hitherto.')
+  }
+  
   return (
     <div className='home-view'>
       <div className='home-view__feed'>
         <ul>
-          {feedItems.map(item => (
+          {feed.length && feed.map(item => (
             <li key={`home-page-feed-${item.name}`}>
               <FeedItem item={item} />
             </li>
           ))}
         </ul>
+        <VisibilitySensor onChange={() => setAppendCount(appendCount + 1)}><div>AM I EVEN VISIBLE??</div></VisibilitySensor>
       </div>
-      <div className='home-view__sidebar'></div>
+      <div className='home-view__sidebar'>
+        {`includeBusinesses: ${includeBusinesses}`}
+        {`includeProducts: ${includeProducts}`}
+        <button onClick={() => setIncludeBusinesses(true)}>Show businesses</button>
+        <button onClick={() => setIncludeBusinesses(true)}>Show businesses</button>
+        <button onClick={() => setAppendCount(appendCount + 1)}>Load more!!</button>
+      </div>
     </div>
   );
 };
@@ -36,6 +77,7 @@ const FeedItem = ({ item }) => {
   return (
     <div className='feed-item'>
       {item.name}
+      {item.id}
     </div>
   );
 };
