700bb046d34c56fb3df41997906ed10eff324e2d
Ben Bolthouse <benbolthouse@gmail.com>
2021-01-31 22:40:36 -0700

    Change dialectOptions in database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -26,6 +26,11 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+          rejectUnauthorized: false
+      }
+    },
     seederStorage: 'sequelize',
   },
 };
